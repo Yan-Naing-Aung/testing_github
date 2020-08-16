1ec0430e66103782f5990707f53d17b5d48a3044
Yauno <69713092+Yan-Naing-Aung@users.noreply.github.com>
2020-08-16 16:29:59 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,13 +2,4 @@
 	echo "Sending Email.";
 	
 	echo "another email sent";
-
-	echo "second line from cli";
-	echo "second line from online";
-
-
-	echo "third line from online";
-	echo "third line from cli";
-	
-	echo "another test by myself";
 ?>
